81b7ddd2535383d7f9cc3bd637124c793e77cd87
新田一斗 <shintaichito@shintaichitonoMacBook-Air.local>
2022-09-28 19:02:41 +0900

    コンテンツ追加

---
+++ js/about.db39d021.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunki_icc_github_io_vue"]=self["webpackChunki_icc_github_io_vue"]||[]).push([[443],{7381:function(u,i,n){n.r(i),n.d(i,{default:function(){return h}});var t=n(3396);const c={class:"about"},e=(0,t._)("h1",null,"This is an about page",-1),a=[e];function r(u,i){return(0,t.wg)(),(0,t.iD)("div",c,a)}var s=n(89);const o={},_=(0,s.Z)(o,[["render",r]]);var h=_}}]);
+//# sourceMappingURL=about.db39d021.js.map
