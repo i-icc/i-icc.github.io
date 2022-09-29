d436515ee9d94685d292f9e7fb09fe1f2574889f
新田一斗 <shintaichito@shintaichitonoMacBook-Air.local>
2022-09-30 01:01:15 +0900

    v0.1.1

---
+++ js/about.00cc75c6.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunki_icc"]=self["webpackChunki_icc"]||[]).push([[443],{7381:function(n,c,u){u.r(c),u.d(c,{default:function(){return l}});var i=u(3396);const t={class:"about"},a=(0,i._)("h1",null,"This is an about page",-1),e=[a];function r(n,c){return(0,i.wg)(),(0,i.iD)("div",t,e)}var s=u(89);const o={},f=(0,s.Z)(o,[["render",r]]);var l=f}}]);
+//# sourceMappingURL=about.00cc75c6.js.map
