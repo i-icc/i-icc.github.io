a6106bada566d8f697867ee52f57fb08de7bb527
i-icc <ichi623110@gmail.com>
2024-09-01 12:00:47 +0900

    info ついか

--- src/views/InfoView.vue
+++ src/views/InfoView.vue
@@ -1,14 +1,13 @@
 <template>
   <main class="container">
     <ul class="list-group-flush">
+      <li class="list-group-item">2024-09-01 お問い合わせ先を追加</li>
+      <li class="list-group-item">2024-03-20 ブログへのリンク追加。</li>
       <li class="list-group-item">
         2022-10-01 ポートフォリオサイトを当サイトへ移動。(<a
           href="https://site-2644830-4822-7140.mystrikingly.com/"
           >前サイト)</a
         >
-      </li>
-      <li class="list-group-item">
-        2024-03-20 ブログへのリンク追加。
       </li>
     </ul>
   </main>
