94ea84d8cb56e5ab7670aa71ac3ff8006fb2f69a
i-icc <ichi623110@gmail.com>
2024-09-01 12:21:11 +0900

    おすすめをトップに表示 &

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,18 +5,27 @@
       <div class="display-8 mb-2">思いついたものを暇な時に作っています</div>
     </div>
 
-    <div class="row">
+    <div class="row border-bottom p-3">
       <div class="col-md-3 display-6 link" v-for="link in links" :key="link.l">
         <router-link class="nav-link" :to="link.l" style="color: black">{{
           link.t
         }}</router-link>
       </div>
     </div>
+
+    <div class="m-5">
+      <div class="display-5"><b>recommend works</b></div>
+      <div class="works">
+        <WorksComponent :works="works" :tag="tag" />
+      </div>
+    </div>
   </main>
 </template>
 
 <script>
+import WorksComponent from "@/components/WorksComponent.vue";
 import "../../public/css/home.css";
+import "../../public/css/works.css";
 
 export default {
   data() {
@@ -27,8 +36,23 @@ export default {
         { l: "/info", t: "Info" },
         { l: "/blog", t: "Blog" },
       ],
-      recommended_ids: [19, 0],
+      works: [],
     };
+  },
+  mounted() {
+    let recommended_ids = [13, 19, 1];
+    recommended_ids.forEach((i) => {
+      fetch(`/works/${i}.json`)
+        .then((response) => {
+          return response.json();
+        })
+        .then((json) => {
+          this.works.push(json);
+        });
+    });
+  },
+  components: {
+    WorksComponent,
   },
 };
 </script>
